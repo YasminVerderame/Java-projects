562f510e8b641e0ff716f31702e76abb4e1223f3
Yasmin Verderame <yasmin_verderame@hotmail.com>
2022-08-01 14:39:22 -0300

    add java project

---
+++ ProjetoPessoa/src/br/fiap/empregado/Empregado.java
@@ -0,0 +1,29 @@
+package br.fiap.empregado;
+
+import br.fiap.pessoa.Pessoa;
+
+public abstract class Empregado extends Pessoa {
+	
+	protected String matricula;
+
+	public Empregado(String nome, String cpf, String matricula) {
+		super(nome, cpf);
+		this.matricula = matricula;
+	}
+	
+	@Override
+	public String toString() {
+		String aux = super.toString();
+		aux += "Matrícula: " + matricula + "\n";
+		return aux;
+	}
+
+	public String getMatricula() {
+		return matricula;
+	}
+
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+		
+}
